d9e6d2fde34bbe16de003e907bd4937bd2cde776
Michael Sheely <msheely@hmc.edu>
2020-05-09 19:07:49 -0700

    Tabs to spaces

--- bfmeReforgedPlayerController.cpp
+++ bfmeReforgedPlayerController.cpp
@@ -9,98 +9,98 @@
 
 AbfmeReforgedPlayerController::AbfmeReforgedPlayerController()
 {
-	bShowMouseCursor = true;
-	DefaultMouseCursor = EMouseCursor::Crosshairs;
+  bShowMouseCursor = true;
+  DefaultMouseCursor = EMouseCursor::Crosshairs;
 }
 
 void AbfmeReforgedPlayerController::PlayerTick(float DeltaTime)
 {
-	Super::PlayerTick(DeltaTime);
+  Super::PlayerTick(DeltaTime);
 
-	// keep updating the destination every tick while desired
-	if (bMoveToMouseCursor)
-	{
-		MoveToMouseCursor();
-	}
+  // keep updating the destination every tick while desired
+  if (bMoveToMouseCursor)
+  {
+    MoveToMouseCursor();
+  }
 }
 
 void AbfmeReforgedPlayerController::SetupInputComponent()
 {
-	// set up gameplay key bindings
-	Super::SetupInputComponent();
+  // set up gameplay key bindings
+  Super::SetupInputComponent();
 
-	InputComponent->BindAction("SetDestination", IE_Pressed, this, &AbfmeReforgedPlayerController::OnSetDestinationPressed);
-	InputComponent->BindAction("SetDestination", IE_Released, this, &AbfmeReforgedPlayerController::OnSetDestinationReleased);
+  InputComponent->BindAction("SetDestination", IE_Pressed, this, &AbfmeReforgedPlayerController::OnSetDestinationPressed);
+  InputComponent->BindAction("SetDestination", IE_Released, this, &AbfmeReforgedPlayerController::OnSetDestinationReleased);
 
-	// support touch devices 
-	InputComponent->BindTouch(EInputEvent::IE_Pressed, this, &AbfmeReforgedPlayerController::MoveToTouchLocation);
-	InputComponent->BindTouch(EInputEvent::IE_Repeat, this, &AbfmeReforgedPlayerController::MoveToTouchLocation);
+  // support touch devices 
+  InputComponent->BindTouch(EInputEvent::IE_Pressed, this, &AbfmeReforgedPlayerController::MoveToTouchLocation);
+  InputComponent->BindTouch(EInputEvent::IE_Repeat, this, &AbfmeReforgedPlayerController::MoveToTouchLocation);
 
 }
 
 void AbfmeReforgedPlayerController::MoveToMouseCursor()
 {
-	if (UHeadMountedDisplayFunctionLibrary::IsHeadMountedDisplayEnabled())
-	{
-		if (AbfmeReforgedCharacter* MyPawn = Cast<AbfmeReforgedCharacter>(GetPawn()))
-		{
-			if (MyPawn->GetCursorToWorld())
-			{
-				UAIBlueprintHelperLibrary::SimpleMoveToLocation(this, MyPawn->GetCursorToWorld()->GetComponentLocation());
-			}
-		}
-	}
-	else
-	{
-		// Trace to see what is under the mouse cursor
-		FHitResult Hit;
-		GetHitResultUnderCursor(ECC_Visibility, false, Hit);
+  if (UHeadMountedDisplayFunctionLibrary::IsHeadMountedDisplayEnabled())
+  {
+    if (AbfmeReforgedCharacter* MyPawn = Cast<AbfmeReforgedCharacter>(GetPawn()))
+    {
+      if (MyPawn->GetCursorToWorld())
+      {
+        UAIBlueprintHelperLibrary::SimpleMoveToLocation(this, MyPawn->GetCursorToWorld()->GetComponentLocation());
+      }
+    }
+  }
+  else
+  {
+    // Trace to see what is under the mouse cursor
+    FHitResult Hit;
+    GetHitResultUnderCursor(ECC_Visibility, false, Hit);
 
-		if (Hit.bBlockingHit)
-		{
-			// We hit something, move there
-			SetNewMoveDestination(Hit.ImpactPoint);
-		}
-	}
+    if (Hit.bBlockingHit)
+    {
+      // We hit something, move there
+      SetNewMoveDestination(Hit.ImpactPoint);
+    }
+  }
 }
 
 void AbfmeReforgedPlayerController::MoveToTouchLocation(const ETouchIndex::Type FingerIndex, const FVector Location)
 {
-	FVector2D ScreenSpaceLocation(Location);
+  FVector2D ScreenSpaceLocation(Location);
 
-	// Trace to see what is under the touch location
-	FHitResult HitResult;
-	GetHitResultAtScreenPosition(ScreenSpaceLocation, CurrentClickTraceChannel, true, HitResult);
-	if (HitResult.bBlockingHit)
-	{
-		// We hit something, move there
-		SetNewMoveDestination(HitResult.ImpactPoint);
-	}
+  // Trace to see what is under the touch location
+  FHitResult HitResult;
+  GetHitResultAtScreenPosition(ScreenSpaceLocation, CurrentClickTraceChannel, true, HitResult);
+  if (HitResult.bBlockingHit)
+  {
+    // We hit something, move there
+    SetNewMoveDestination(HitResult.ImpactPoint);
+  }
 }
 
 void AbfmeReforgedPlayerController::SetNewMoveDestination(const FVector DestLocation)
 {
-	APawn* const MyPawn = GetPawn();
-	if (MyPawn)
-	{
-		float const Distance = FVector::Dist(DestLocation, MyPawn->GetActorLocation());
+  APawn* const MyPawn = GetPawn();
+  if (MyPawn)
+  {
+    float const Distance = FVector::Dist(DestLocation, MyPawn->GetActorLocation());
 
-		// We need to issue move command only if far enough in order for walk animation to play correctly
-		if ((Distance > 120.0f))
-		{
-			UAIBlueprintHelperLibrary::SimpleMoveToLocation(this, DestLocation);
-		}
-	}
+    // We need to issue move command only if far enough in order for walk animation to play correctly
+    if ((Distance > 120.0f))
+    {
+      UAIBlueprintHelperLibrary::SimpleMoveToLocation(this, DestLocation);
+    }
+  }
 }
 
 void AbfmeReforgedPlayerController::OnSetDestinationPressed()
 {
-	// set flag to keep updating destination until released
-	bMoveToMouseCursor = true;
+  // set flag to keep updating destination until released
+  bMoveToMouseCursor = true;
 }
 
 void AbfmeReforgedPlayerController::OnSetDestinationReleased()
 {
-	// clear flag to indicate we should stop updating the destination
-	bMoveToMouseCursor = false;
+  // clear flag to indicate we should stop updating the destination
+  bMoveToMouseCursor = false;
 }
